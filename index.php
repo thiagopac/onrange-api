686baa5299e63fda5e45c6b14615f908f5facccc
Paulo Felipe <paulofelipe@gmail.com>
2014-11-30 13:52:41 -0200

    Acerto do listaLocaisRange. O campo DESTAQUE estava escrito errado em um dos selects.

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ function listaLocaisRange($latitude_atual,$longitude_atual,$range,$order_by)
 					acos(sin(:latitude_atual)*sin(radians(latitude)) + cos(:latitude_atual)*cos(radians(latitude))*cos(radians(longitude)-:longitude_atual)) * 6371 As distancia,
 					qt_checkin, id_tipo_local AS tipo_local, destaque
 				FROM (
-						SELECT LOCAL.id_local, LOCAL.nome, LOCAL.latitude, LOCAL.longitude, CHECKINS_CORRENTES.qt_checkin, LOCAL.id_tipo_local, LOCAL.destque
+						SELECT LOCAL.id_local, LOCAL.nome, LOCAL.latitude, LOCAL.longitude, CHECKINS_CORRENTES.qt_checkin, LOCAL.id_tipo_local, LOCAL.destaque
 						FROM LOCAL JOIN CHECKINS_CORRENTES ON LOCAL.id_local = CHECKINS_CORRENTES.id_local
 						WHERE
 						LOCAL.dt_exclusao IS NULL
